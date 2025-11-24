afc6e5fe6d52f39072d9c1db6e0170157101f662
byBit-ovo <q3302371508@gmail.com>
2025-11-24 11:52:48 +0800

    cmake

---
+++ mini_demo/cmake_hello/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <assert.h>
+
+int main(){
+	assert(1);
+	std::cout << "hello" << std::endl;
+	return 0;
+}
--- test.py
+++ test.py
@@ -2,3 +2,4 @@ a = [1,2,3]
 a.pop(-1)
 print(a)
 
+import json
