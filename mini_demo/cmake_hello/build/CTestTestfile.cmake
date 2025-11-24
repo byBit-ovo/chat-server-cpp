e59223b4887683bd76a4ab0d11dcb3bc96ce1a22
byBit-ovo <q3302371508@gmail.com>
2025-11-24 11:55:03 +0800

    tidy

--- mini_demo/cmake_hello/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/qzr/gitee/Micro_communication/mini_demo/cmake_hello
-# Build directory: /home/qzr/gitee/Micro_communication/mini_demo/cmake_hello/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Name "testAdd" "COMMAND" "main")
-set_tests_properties(Name PROPERTIES  _BACKTRACE_TRIPLES "/home/qzr/gitee/Micro_communication/mini_demo/cmake_hello/CMakeLists.txt;12;add_test;/home/qzr/gitee/Micro_communication/mini_demo/cmake_hello/CMakeLists.txt;0;")
