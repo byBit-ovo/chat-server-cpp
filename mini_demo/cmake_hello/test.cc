07a37ff87e30c9937bd45d3e0fe69ecbe37eb6bb
byBit-ovo <q3302371508@gmail.com>
2025-11-27 19:06:02 +0800

    cmk

--- mini_demo/cmake_hello/test.cc
+++ mini_demo/cmake_hello/test.cc
@@ -2,7 +2,8 @@
 #include <assert.h>
 
 int main(){
-	assert(1==1);
-	std::cout << "hello" << std::endl;
+	// assert(1+1==2);
+	// assert(1+1==4);
+	// assert(0);
 	return 0;
 }
--- test.py
+++ test.py
@@ -3,3 +3,7 @@ a.pop(-1)
 print(a)
 
 import json
+
+s = "001260"
+a = int(s)
+print(a + 1)
